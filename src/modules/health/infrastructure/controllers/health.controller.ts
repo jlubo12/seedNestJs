596df68cc75ee1a2ed5f11a44c1ce6ba6c7dfc36
“Juan <juanpablolubo@gmail.comecho Hello>
2023-11-08 16:25:49 -0300

    fix health path

--- src/modules/health/infrastructure/controllers/health.controller.ts
+++ src/modules/health/infrastructure/controllers/health.controller.ts
@@ -1,21 +1,14 @@
 import { Controller, Get, HttpCode, VERSION_NEUTRAL } from '@nestjs/common';
 
 @Controller({
-  path: 'health',
+  path: '',
   version: VERSION_NEUTRAL,
 })
 export class HealthController {
   /** Get the liveness probe response */
-  @Get('liveness')
+  @Get()
   @HttpCode(200)
   getLivenessProbe(): string {
-    return 'OK';
-  }
-
-  /** Get the readiness probe response */
-  @Get('readiness')
-  @HttpCode(200)
-  getReadinessProbe(): string {
     return 'OK';
   }
 }
